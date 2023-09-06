03853497c992b4426791af5cc96b99451559b590
UsamaWaqas <usamawaqas.m@gmail.com>
2023-09-06 23:30:30 +0500

    updated

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+import Header from './Components/Layouts/Header'
+import Meals from './Components/Meals/Meals'
+import Cart from './Components/Cart/Cart';
+import CartProvider from './store/CartProvider';
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
+  return (
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
